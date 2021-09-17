7b74e3a57ea5572cd3390744d2ddaabc38748a5e
Yibo-Zhang <mucherzhang@hotmail.com>
2021-09-17 13:06:39 -0400

    update

--- python/heisenberg/main.py
+++ python/heisenberg/main.py
@@ -219,32 +219,107 @@ class New_direction_model:
         quiver = ax.quiver(x_position, y_position, m_x, m_y,color=color_map,
                            angles='xy', pivot='mid', scale=10)
 
-
-    
 class Up_down_flip_model(New_direction_model):
     def __init__(self,N,step,label='simple flip model'):
-        super().__init__(N,step,label)
+        super().__init__(N,step,label='up down random flip')
     def init_grid(self,N):
         theta = np.random.randint(2,size=(N,N))*math.pi
         theta = theta[np.newaxis, :]
         beta = np.zeros((N,N))
         beta = beta[np.newaxis, :]
         return np.concatenate((theta,beta),axis=0)
-
-    def flipping_random_new_direction(self,lamda):
+        
+    def flipping_random_new_direction(self,lamda,temperature):
         '''Monte Carlo move using Metropolis algorithm '''
         for i in range(self.N):
             for j in range(self.N):
                 # 产生0到N-1的随机数
                 a = np.random.randint(0, self.N)
                 b = np.random.randint(0, self.N)
-                new_theta = np.random.randint(2)*math.pi # 0 to pi
+                old_theta = self.config[0,a,b]
+                new_theta = (old_theta/math.pi+1)%2*math.pi # 0 to pi
                 new_beta = 0 # 0
+
                 origin_energy = super().calculate_single_energy(self.config, a, b, self.alpha,lamda)
                 later_energy = super().calculate_new_direction_energy(self.config,a,b,self.alpha,new_theta,new_beta,lamda)
-                cost = later_energy - origin_energy
+                cost = 2*(later_energy - origin_energy) *5
                 # 如果能量降低接受翻转
                 if cost < 0:
                     self.config[:,a,b] = [new_theta,new_beta]
+                elif rand() < np.exp(-cost/temperature):
+                    self.config[:,a,b] = [new_theta,new_beta]
+    def simulation(self,lamda = 0,temperature=2):
+        Energy = []  # 内能
+        # 开始模拟
+        time_start = time.time()
+        for i in range(self.step):
+            self.flipping_random_new_direction(lamda = lamda,temperature=temperature)
+            e = self.calculate_total_energy(self.config,self.alpha,lamda=0)
+            Energy.append(e)
+            if i % 300 == 0:
+                print("已完成第%d步模拟" % i)
+        time_end = time.time()
+        print('totally cost', time_end-time_start)
+        average_energy = [i/np.square(self.N) for i in Energy]
+        self.energy = Energy
+        plt.plot(average_energy,label=self.label)
+        plt.legend()
+
+
+
+
+
     
+class Up_down_flip_model_with_one_alpha(New_direction_model):
+    def __init__(self,N,step,label='simple flip model'):
+        self.N = N
+        self.step=step
+        self.config = self.init_grid(N)
+        self.alpha = self.init_alpha_ones(N)
+        self.label = label
+    def init_grid(self,N):
+        theta = np.random.randint(2,size=(N,N))*math.pi
+        theta = theta[np.newaxis, :]
+        beta = np.zeros((N,N))
+        beta = beta[np.newaxis, :]
+        return np.concatenate((theta,beta),axis=0)
+    def init_alpha_ones(self,N):
+        return np.ones([2,N,N])
+        
+    def flipping_random_new_direction(self,lamda,temperature):
+        '''Monte Carlo move using Metropolis algorithm '''
+        for i in range(self.N):
+            for j in range(self.N):
+                # 产生0到N-1的随机数
+                a = np.random.randint(0, self.N)
+                b = np.random.randint(0, self.N)
+                old_theta = self.config[0,a,b]
+                new_theta = (old_theta/math.pi+1)%2*math.pi # 0 to pi
+                new_beta = 0 # 0
+
+                origin_energy = super().calculate_single_energy(self.config, a, b, self.alpha,lamda)
+                later_energy = super().calculate_new_direction_energy(self.config,a,b,self.alpha,new_theta,new_beta,lamda)
+                cost = 2*(later_energy - origin_energy)
+                # 如果能量降低接受翻转
+                if cost < 0:
+                    self.config[:,a,b] = [new_theta,new_beta]
+                elif rand() < np.exp(-cost/temperature):
+                    self.config[:,a,b] = [new_theta,new_beta]
+    def simulation(self,lamda = 0,temperature=2):
+        Energy = []  # 内能
+        # 开始模拟
+        time_start = time.time()
+        for i in range(self.step):
+            self.flipping_random_new_direction(lamda = lamda,temperature=temperature)
+            e = self.calculate_total_energy(self.config,self.alpha,lamda=0)
+            Energy.append(e)
+            if i % 300 == 0:
+                print("已完成第%d步模拟" % i)
+        time_end = time.time()
+        print('totally cost', time_end-time_start)
+        average_energy = [i/np.square(self.N) for i in Energy]
+        self.energy = Energy
+        plt.plot(average_energy,label=self.label)
+        plt.legend()
+
 
